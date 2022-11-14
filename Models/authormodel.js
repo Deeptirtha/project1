e29a51be61040d7371d89ae7e66f520a381d380d
jagriti89 <jagritisinghpro@gmail.com>
2022-11-14 13:09:22 +0530

    author model

---
+++ Models/authormodel.js
@@ -0,0 +1,27 @@
+const mongoose=require('mongoose');
+const authorSchema=new mongoose.Schema({
+     fname:{
+        type: String ,
+        require:true
+    },
+     lname: {
+        type:String,
+        require:true
+    }, 
+    title: {
+        type:string,
+        require:true, 
+        enum:["Mr", "Mrs", "Miss"]}, 
+        email: {
+           type:String,
+           require:true,
+             
+            lowercase:true
+            },
+     password: {
+        type:String,
+        require:true}
+     },{timestemps:true}
+)
+
+module.exports =mongoose.model('Author',authorSchema)
