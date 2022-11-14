3cd993a16ddadc295e08e8969076d4cc393824be
Deeptirtha <tirtha1111222@gmail.com>
2022-11-14 20:26:33 +0530

    14-Nov

--- controller/authercontroller.js
+++ controller/authercontroller.js
@@ -4,8 +4,18 @@ const createAuthor= async function (req, res) {
 
     try{
     let {fname,lname,title,email,password} = req.body
+    let body = req.body
+    let bodydata=Object.keys(body)
+    if(bodydata.length==0){return res.status(400).send({status: false, msg:"body is empty" })}
+    
+    if(!fname){ return res.status(400).send({status: false, msg:"firstname is mandatary" })}
+    if(!lname){ return res.status(400).send({status: false, msg:"lasttname is mandatary" })}
+    if(!title){ return res.status(400).send({status: false, msg:"title is mandatary" })}
+    if(!email){ return res.status(400).send({status: false, msg:"email is mandatary" })}
+    if(!password){ return res.status(400).send({status: false, msg:"password is manda"})}
 
-    if(!fname || !lname || !title ||!email|| !password){res.status(400).send({status: false, msg:"all data is mandatary" })}
+
+    
 
     function validateEmail(mail) {
         var re = /\S+@\S+\.\S+/;
--- controller/blogcontroller.js
+++ controller/blogcontroller.js
@@ -5,6 +5,14 @@ const AuthorModel= require("../Models/authormodel")
 const createBlog= async function (req, res) {
     try{
     let {title,body,authorId,category} = req.body
+    let sendbody = req.body
+    let bodydata=Object.keys(sendbody)
+    if(bodydata.length==0){return res.status(400).send({status: false, msg:"body is empty" })}
+
+    if(!title){return res.status(400).send({status:false,msg:"title is mandatory"})}
+    if(!body){return res.status(400).send({status:false,msg:"body is mandatory"})}
+    if(!authorId){return res.status(400).send({status:false,msg:"authorld is mandatory"})}
+    if(!category){return res.status(400).send({status:false,msg:"category is mandatory"})}
 
     if(!title||!body||!authorId||!category){return res.status(400).send({status:false,msg:"all field is mandatory"})}
     if(!isValidObjectId(authorId)){return res.status(400).send({status:false,msg:"author id is not valid"})}
@@ -25,12 +33,48 @@ try{
     let authQuery=req.query.authorId
     let cata=req.query.category
     let tag=req.query.tag
-    let subcategory=req.query.subcategory
+    let subcata=req.query.subcategory
+if (!tag && !subcata && !authQuery && !cata){
     let blogs = await blogModel.find().populate('authorId')
+    return res.status(200).send({status:true,msg:blogs})
+    }
+    
+if(!tag && !subcata){
+    if(!authQuery){return res.status(400).send({status:false,msg:"author id is not present"})}
+    if(!cata){return res.status(400).send({status:false,msg:"category is not present"})}
+
+    if(!isValidObjectId(authQuery)){return res.status(400).send({status:false,msg:"author id is not valid"})}
+
+    let authorchk = await AuthorModel.find({_id:authQuery})
+    if(authorchk.length<1) {return res.status(400).send({status:false,msg:"author id is not present in db"})}
+
+    let categorychk = await blogModel.find({category:cata})
+    if(categorychk.length<1) {return res.status(400).send({status:false,msg:"no such category present in db"})}
+
+    let blogs = await blogModel.find({authorId:authQuery,category:cata}).populate('authorId')
+
+   return res.status(200).send({status:true,msg:blogs})
+
+}
+if(!authQuery && !cata){
+   if(!tag){return res.status(400).send({status:false,msg:"tag is not present"})}
+   if(!subcata){return res.status(400).send({status:false,msg:"subcata is not present"})}
 
 
+    let tagchk = await blogModel.find({tags:tag})
+    if(tagchk.length<1) {return res.status(400).send({status:false,msg:"no such tag present in db"})}
 
-    res.status(200).send({status:true,msg:blogs})
+    let subcategorychk = await blogModel.find({subcategory:subcata})
+    if(subcategorychk.length<1) {return res.status(400).send({status:false,msg:"no such tag present in db"})}
+
+    let blogs = await blogModel.find({tags:tag,subcategory:subcata}).populate('authorId')
+
+  return   res.status(200).send({status:true,msg:blogs})
+
+}
+
+
+else{ return res.status(400).send({status:false,msg:"enter valid paid of query"})}
 
 }
 catch(error){
